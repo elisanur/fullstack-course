12bd6c06fc1f25bc30819a14888298a1e75b6c81
Elisa Nurmi <nurmi.elisa@gmail.com>
2019-09-28 00:12:19 +0300

    creating docs file for github pages and adding countries build there

---
+++ docs/precache-manifest.fa1a46488e070c31be478d42b85dddc7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29e391d2065bd9b749081b299865c62d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "279c8c02253695c539a7",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "c019b885bee3577cf0ec",
+    "url": "/static/js/2.51aeefe4.chunk.js"
+  },
+  {
+    "revision": "279c8c02253695c539a7",
+    "url": "/static/js/main.51a7aa79.chunk.js"
+  },
+  {
+    "revision": "5c28a4bea5ee5548a554",
+    "url": "/static/js/runtime~main.fd87a315.js"
+  }
+]);
